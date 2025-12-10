b5b4dedf1211c731b2d55cdc4d0f57a7a18ba082
dudwntjs <sts07190@naver.com>
2025-12-10 13:33:01 +0900

    [setting] 프로젝트 생성

---
+++ Baemin/Baemin/BaeminApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaeminApp.swift
+//  Baemin
+//
+//  Created by sun on 12/10/25.
+//
+
+import SwiftUI
+
+@main
+struct BaeminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
